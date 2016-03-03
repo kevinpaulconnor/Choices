bbb3f2106158fe48f64584736d20c739a37b4432
Kevin Connor <kevinpaulconnor@gmail.com>
2016-03-03 15:28:48 -0800

    stub itemChooser layout; container/child relation

--- PreferencePicker/ChoosePreferenceViewController.swift
+++ PreferencePicker/ChoosePreferenceViewController.swift
@@ -9,17 +9,23 @@
 import UIKit
 
 class ChoosePreferenceViewController: UIViewController {
-
+    var activeSet: PreferenceSet?
+    @IBOutlet weak var topItemView: UIView!
+    let itemsToDisplay = 2
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        let topItemViewController = storyboard?.instantiateViewControllerWithIdentifier("iTunesItemController") as! iTunesItemChooserViewController
+        self.addChildViewController(topItemViewController)
+        topItemViewController.view.frame = CGRectMake(0, 0, self.topItemView.frame.size.width, self.topItemView.frame.size.height)
+        self.topItemView.addSubview(topItemViewController.view)
+        topItemViewController.didMoveToParentViewController(self)
+
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
-
