73d9612d4b5bbc226ced351a756c60ce32facb5b
Kevin Connor <kevinpaulconnor@gmail.com>
2016-03-02 16:55:52 -0800

    introduce PreferenceSetOperations superclass
    stub PSO.getItemsForComparison

--- PreferencePicker/PreferenceSet.swift
+++ PreferencePicker/PreferenceSet.swift
@@ -9,17 +9,30 @@
 import Foundation
 import MediaPlayer
 
+// PreferenceSet protocol provides APIs for the presentation
+// and modification of PreferenceSet in view controllers
 protocol PreferenceSet {
     var items: [PreferenceSetItem] { get set }
     var imported: Bool { get set }
     var created: Bool { get set }
     var title: String {get set }
     
-    
 }
 
+// PreferenceSetOperations holds common logic for determining
+// and storing user preferences about the items in the set
+class PreferenceSetOperations {
+    
+    func getItemsForComparison(numberToGet: Int) -> [PreferenceSetItem] {
+        
+        return [PreferenceSetItem]()
+    }
+}
+
+// Preference Sets should conform to PreferenceSet, and subclass PreferenceSetOperator
+
 // would love to figure out how to classname this programmatically, e.g. PreferenceSetTypeIds.iTunesPlaylist
-class iTunesPlaylistPreferenceSet : PreferenceSet {
+class iTunesPlaylistPreferenceSet : PreferenceSetOperations, PreferenceSet {
     var items = [PreferenceSetItem]()
     var imported = iTunesPreferenceSetType.importable
     var created = iTunesPreferenceSetType.creatable
