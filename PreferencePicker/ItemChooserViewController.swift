e14320a4d853704f67cb31421d61367d026cf63c
Kevin Connor <kevinpaulconnor@gmail.com>
2016-03-03 15:27:03 -0800

    base itemChooserVC and iTunes extension

---
+++ PreferencePicker/ItemChooserViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ItemChooserViewController.swift
+//  PreferencePicker
+//
+//  Created by Kevin Connor on 3/3/16.
+//  Copyright © 2016 Kevin Connor. All rights reserved.
+//
+
+import UIKit
+
+class ItemChooserViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
+class iTunesItemChooserViewController : ItemChooserViewController {
+    
+}
