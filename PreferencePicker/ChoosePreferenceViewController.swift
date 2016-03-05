7e9eae42fdeaae48fbb0e4aa1859c7e6cbbfb033
Kevin Connor <kevinpaulconnor@gmail.com>
2016-03-04 18:47:55 -0800

    an attempt at generalizing chooserItemController code into containers

--- PreferencePicker/ChoosePreferenceViewController.swift
+++ PreferencePicker/ChoosePreferenceViewController.swift
@@ -11,17 +11,41 @@ import UIKit
 class ChoosePreferenceViewController: UIViewController {
     var activeSet: PreferenceSet?
     @IBOutlet weak var topItemView: UIView!
+    @IBOutlet weak var bottomItemView: UIView!
     let itemsToDisplay = 2
-    
     override func viewDidLoad() {
+
         super.viewDidLoad()
+        self.setContainerView(topItemView)
+        self.setContainerView(bottomItemView)
 
-        let topItemViewController = storyboard?.instantiateViewControllerWithIdentifier("iTunesItemController") as! iTunesItemChooserViewController
-        self.addChildViewController(topItemViewController)
-        topItemViewController.view.frame = CGRectMake(0, 0, self.topItemView.frame.size.width, self.topItemView.frame.size.height)
-        self.topItemView.addSubview(topItemViewController.view)
-        topItemViewController.didMoveToParentViewController(self)
 
+    }
+    
+    private struct vcGenerators {
+        static let music = {(myVC: ChoosePreferenceViewController) -> UIViewController in
+                return myVC.storyboard?.instantiateViewControllerWithIdentifier("iTunesItemController") as! iTunesItemChooserViewController
+        }
+    }
+    
+    func setContainerView(containerView: UIView) {
+        let itemViewController = self.chooserItemControllersByPreferenceSetType()
+        self.addChildViewController(itemViewController)
+        itemViewController.view.frame = CGRectMake(0, 0, containerView.frame.size.width, containerView.frame.size.height)
+        containerView.addSubview(itemViewController.view)
+        itemViewController.didMoveToParentViewController(self)
+    }
+    
+    // preference set type tells the view what kind of view controllers to load
+    private func chooserItemControllersByPreferenceSetType () -> (UIViewController) {
+        var controllerIdentifier: UIViewController?
+        switch activeSet!.preferenceSetType {
+        case PreferenceSetTypeIds.iTunesPlaylist:
+            controllerIdentifier = vcGenerators.music(self)
+        default: break
+        }
+        
+        return controllerIdentifier!
     }
 
     override func didReceiveMemoryWarning() {
