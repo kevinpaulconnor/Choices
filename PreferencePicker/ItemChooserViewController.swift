27a2908254fc286d8fbb192513d1d4d3518e214f
Kevin Connor <kevinpaulconnor@gmail.com>
2016-04-15 14:41:57 -0700

    some code cleanup

--- PreferencePicker/ItemChooserViewController.swift
+++ PreferencePicker/ItemChooserViewController.swift
@@ -10,21 +10,7 @@ import UIKit
 import MediaPlayer
 
 class ItemChooserViewController: UIViewController {
-
-    
     var item: PreferenceSetItem?
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        mediaItem = item!
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
 }
 
 class iTunesItemChooserViewController : ItemChooserViewController {
